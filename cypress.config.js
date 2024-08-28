c62e00a3fbf71f10ea562eeb9e1938fae8d3332a
Ram Gangrade <gangraderam538@gmail.com>
2024-08-28 16:57:58 +0530

    Initial commit of Cypress project

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('cypress');
+const cucumber = require('cypress-cucumber-preprocessor').default;
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      on('file:preprocessor', cucumber());
+      // Other setup options can be added here
+    },
+    specPattern: 'CypressCucumberPro/cypress/e2e/features/*.feature',  // Adjusted path
+    supportFile: 'CypressCucumberPro/cypress/support/e2e.js',  // Path to support file
+    // "nonGlobalStepDefinitions": true,
+    // "stepDefinitions": "CypressCucumberPro/cypress/e2e/stepDefinitions",
+  },
+});
